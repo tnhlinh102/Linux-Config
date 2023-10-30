85240e66a18cc1269c8fce2ffb7da47f86c7ee98
tnhlinh102 <81002713+tnhlinh102@users.noreply.github.com>
2023-10-30 09:53:32 +0700

    Update fugitive.lua keymaps

--- nvim/lua/init/plugins/fugitive.lua
+++ nvim/lua/init/plugins/fugitive.lua
@@ -3,32 +3,14 @@ return {
   config = function()
     local keymap = vim.keymap
     keymap.set("n", "<leader>gs", vim.cmd.Git)
-    local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
-
-    local autocmd = vim.api.nvim_create_autocmd
-    autocmd("BufWinEnter", {
-      group = ThePrimeagen_Fugitive,
-      pattern = "*",
-      callback = function()
-          if vim.bo.ft ~= "fugitive" then
-              return
-          end
-
-          local bufnr = vim.api.nvim_get_current_buf()
-          local opts = {buffer = bufnr, remap = false}
-          keymap.set("n", "<leader>bu", function()
+    keymap.set("n", "<leader>ps", function()
               vim.cmd.Git('push')
-          end, opts)
-
-          -- rebase always
-          keymap.set("n", "<leader>bd", function()
+          end)
+    keymap.set("n", "<leader>pl", function()
               vim.cmd.Git({'pull',  '--rebase'})
-          end, opts)
-
-          -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-          -- needed if i did not set the branch up correctly
-          vim.keymap.set("n", "<leader>bU", ":Git push -u origin ", opts);
-      end,
-  })
+          end)
+    keymap.set("n", "<leader>pL", ":Git push -u origin ");
+    keymap.set("n", "<leader>rt", ":Git restore .");
+    keymap.set("n", "<leader>rs", ":Git reset");
   end,
 }
