6921b9fcbc24d14d3a4c1af6fb5c248784b28717
tnhlinh102 <81002713+tnhlinh102@users.noreply.github.com>
2023-10-31 14:54:45 +0700

    Update any-jump.lua keymap

--- nvim/lua/init/plugins/any-jump.lua
+++ nvim/lua/init/plugins/any-jump.lua
@@ -4,9 +4,9 @@ return {
     
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>j", "AnyJump", { desc = "Jump to definition under cursor" })
-    keymap.set("v", "<leader>j", "AnyJumpVisual", { desc = "Jump to selected text in visual mode" })
-    keymap.set("n", "<leader>b", ":AnyJumpBack", { desc = "Open previous opened file (after jump)" })
-    keymap.set("n", "<leader>al", "AnyJumpLastResults", { desc = "Open last closed search window again" })
+    keymap.set("n", "<leader>j", ":AnyJump <CR>", { desc = "Jump to definition under cursor" })
+    keymap.set("v", "<leader>j", ":AnyJumpVisual <CR>", { desc = "Jump to selected text in visual mode" })
+    keymap.set("n", "<leader>b", ":AnyJumpBack <CR>", { desc = "Open previous opened file (after jump)" })
+    keymap.set("n", "<leader>al", ":AnyJumpLastResults <CR>", { desc = "Open last closed search window again" })
   end,
 }
