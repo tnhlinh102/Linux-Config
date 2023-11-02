03b78cdcb64ffec1cf6ac70cafe37312a95522ec
tnhlinh102 <81002713+tnhlinh102@users.noreply.github.com>
2023-11-02 09:44:02 +0700

    Update fugitive.lua keymap

--- nvim/lua/init/plugins/fugitive.lua
+++ nvim/lua/init/plugins/fugitive.lua
@@ -5,7 +5,7 @@ return {
     keymap.set("n", "<leader>gs", vim.cmd.Git)
     keymap.set("n", "<leader>ps", ":Git push <CR>")
     keymap.set("n", "<leader>pl", ":Git pull --rebase <CR>")
-    keymap.set("n", "<leader>pL", ":Git push -u origin <CR>");
+    keymap.set("n", "<leader>pS", ":Git push -u origin <CR>");
     keymap.set("n", "<leader>rt", ":Git restore . <CR>");
     keymap.set("n", "<leader>rs", ":Git reset <CR>");
   end,
